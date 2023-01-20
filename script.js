64f8f3682efa4c191c8002d074846680b779a022
Vivian Sanchez Desktop <visasal88@gmail.com>
2023-01-20 17:45:49 -0300

    aula 4 finalizada

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-  run: ['01-01', '01-02', '01-03'],
-  water: ['01-01', '01-02', '01-03', '01-04', '01-05', '01-06'],
-  exercise: ['01-02', '01-04', '01-06'],
-  sleep: ['01-01', '01-02', '01-03', '01-04', '01-05', '01-06'],
-  walkDog: ['01-03', '01-06'],
-  healthyFood: ['01-02', '01-04', '01-06']
+button.addEventListener('click', add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if(dayExists) {
+    alert("Dia já incluso")
+    return
+  } 
+
+  alert('Adicionado com sucesso!')
+  nlwSetup.addDay(today)
+
 }
 
-nlwSetup.setData(data)
+function save() {
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
 
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
+nlwSetup.setData(data)
 nlwSetup.load()
